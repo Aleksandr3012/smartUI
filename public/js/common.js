cbdd8e1f4a9af1ba87324b714d0d6f1ab2cd3211
Aleksandr3012 <flaming541@gmail.com>
2021-10-16 08:50:33 +0300

    done

--- public/js/common.js
+++ public/js/common.js
@@ -7,7 +7,6 @@ const JSCCommon = {
 
 	modalCall() {
 		const link = ".link-modal-js";
-
 		Fancybox.bind(link, {
 			arrows: false,
 			infobar: false,
@@ -41,7 +40,6 @@ const JSCCommon = {
 							el.tagName == "INPUT"
 								? el.value = val
 								: el.innerHTML = val;
-							// console.log(modal.querySelector(elem).tagName)
 						}
 					}
 					setValue(data.title, '.ttu');
@@ -91,123 +89,11 @@ const JSCCommon = {
 	},
 	// /mobileMenu
 
-	// tabs  .
-	// tabscostume(tab) {
-
-	// 	$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-	// 		$(this)
-	// 			.addClass('active').siblings().removeClass('active')
-	// 			.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-	// 			.eq($(this).index()).fadeIn().addClass('active');
-	// 	});
-	// },
-	// /tabs
-
-	// inputMask() {
-	// 	// mask for input
-	// 	let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
-	// 	InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
-	// 	Inputmask("+9(999)999-99-99").mask(InputTel);
-	// },
-	// /inputMask
-	// ifie() {
-	// 	var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-	// 	if (isIE11) {
-	// 		document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
-	// 	}
-	// },
-	// sendForm() {
-	// 	var gets = (function () {
-	// 		var a = window.location.search;
-	// 		var b = new Object();
-	// 		var c;
-	// 		a = a.substring(1).split("&");
-	// 		for (var i = 0; i < a.length; i++) {
-	// 			c = a[i].split("=");
-	// 			b[c[0]] = c[1];
-	// 		}
-	// 		return b;
-	// 	})();
-	// 	// form
-	// 	$(document).on('submit', "form", function (e) {
-	// 		e.preventDefault();
-	// 		const th = $(this);
-	// 		var data = th.serialize();
-	// 		th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
-	// 		th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
-	// 		th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
-	// 		th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
-	// 		$.ajax({
-	// 			url: 'action.php',
-	// 			type: 'POST',
-	// 			data: data,
-	// 		}).done(function (data) {
-
-	// 			Fancybox.close();
-	// 			Fancybox.show([{ src: "#modal-thanks", type: "inline" }]);
-	// 			// window.location.replace("/thanks.html");
-	// 			setTimeout(function () {
-	// 				// Done Functions
-	// 				th.trigger("reset");
-	// 				// $.magnificPopup.close();
-	// 				// ym(53383120, 'reachGoal', 'zakaz');
-	// 				// yaCounter55828534.reachGoal('zakaz');
-	// 			}, 4000);
-	// 		}).fail(function () { });
-
-	// 	});
-	// },
-	// heightwindow() {
-	// 	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-	// 	let vh = window.innerHeight * 0.01;
-	// 	// Then we set the value in the --vh custom property to the root of the document
-	// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-	// 	// We listen to the resize event
-	// 	window.addEventListener('resize', () => {
-	// 		// We execute the same script as before
-	// 		let vh = window.innerHeight * 0.01;
-	// 		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	// 	}, { passive: true });
-	// },
-	// animateScroll() {
-	// 	$(document).on('click', " .menu li a, .scroll-link", function () {
-	// 		const elementClick = $(this).attr("href");
-	// 		if (!document.querySelector(elementClick)) {
-	// 			$(this).attr("href", '/' + elementClick)
-	// 		}
-	// 		else {
-	// 			let destination = $(elementClick).offset().top;
-	// 			$('html, body').animate({ scrollTop: destination - 80 }, 0);
-	// 			return false;
-	// 		}
-	// 	});
-	// },
 	getCurrentYear(el) {
 		let now = new Date();
 		let currentYear = document.querySelector(el);
 		if (currentYear) currentYear.innerText = now.getFullYear();
 	},
-	// toggleShow(toggle, drop) {
-
-	// 	let catalogDrop = drop;
-	// 	let catalogToggle = toggle;
-
-	// 	$(document).on('click', catalogToggle, function () {
-	// 		$(this).toggleClass('active').next().fadeToggle('fast', function () {
-	// 			$(this).toggleClass("active")
-	// 		});
-	// 	})
-
-	// 	document.addEventListener('mouseup', (event) => {
-	// 		let container = event.target.closest(catalogDrop + ".active"); // (1)
-	// 		let link = event.target.closest(catalogToggle); // (1)
-	// 		if (!container || !catalogToggle) {
-	// 			$(catalogDrop).removeClass('active').fadeOut();
-	// 			$(catalogToggle).removeClass('active');
-	// 		};
-	// 	}, { passive: true });
-	// }
 };
 // const $ = jQuery;
 
@@ -253,39 +139,89 @@ function eventHandler() {
 
 	whenResize();
 
+	// let defaultSl = {
+	// 	spaceBetween: 0,
+	// 	lazy: {
+	// 		loadPrevNext: true,
+	// 	},
+	// 	watchOverflow: true,
+	// 	spaceBetween: 0,
+	// 	loop: true,
+	// 	navigation: {
+	// 		nextEl: '.swiper-button-next',
+	// 		prevEl: '.swiper-button-prev',
+	// 	},
+	// 	pagination: {
+	// 		el: ' .swiper-pagination',
+	// 		type: 'bullets',
+	// 		clickable: true,
+	// 		// renderBullet: function (index, className) {
+	// 		// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+	// 		// }
+	// 	},
+	// }
 
-	let defaultSl = {
-		spaceBetween: 0,
-		lazy: {
-			loadPrevNext: true,
-		},
-		watchOverflow: true,
-		spaceBetween: 0,
+	const swiper4 = new Swiper('.sSlider__slider--js', {
 		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
 		pagination: {
-			el: ' .swiper-pagination',
+			el: '.sSlider .swiper-pagination',
 			type: 'bullets',
 			clickable: true,
-			// renderBullet: function (index, className) {
-			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
-			// }
 		},
-	}
-
-	const swiper4 = new Swiper('.sBanners__slider--js', {
-		// slidesPerView: 5,
-		...defaultSl,
-		slidesPerView: 'auto',
-		freeMode: true,
-		loopFillGroupWithBlank: true,
-		touchRatio: 0.2,
-		slideToClickedSlide: true,
-		freeModeMomentum: true,
 	});
+
+	var options = {
+		useEasing : true,
+		useGrouping : true,
+		separator : ',',
+		decimal : ','
+	}
+	var count1 = new countUp("count1--js", 0, 23567, 0, 3, options);
+	var count2 = new countUp("count2--js", 0, 431729, 0, 3, options);
+	var count3 = new countUp("count3--js", 0, 892173, 0, 3, options);
+	var count4 = new countUp("count4--js", 0, 56581, 0, 3, options);
+	var count5 = new countUp("count5--js", 0, 3182, 0, 3, options);
+
+
+
+	var counterBlock = document.querySelector('.countWrap--js');
+
+	var Visible = function (target) {
+		// Все позиции элемента
+		var targetPosition = {
+				top: window.pageYOffset + target.getBoundingClientRect().top,
+				left: window.pageXOffset + target.getBoundingClientRect().left,
+				right: window.pageXOffset + target.getBoundingClientRect().right,
+				bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+			},
+			// Получаем позиции окна
+			windowPosition = {
+				top: window.pageYOffset,
+				left: window.pageXOffset,
+				right: window.pageXOffset + document.documentElement.clientWidth,
+				bottom: window.pageYOffset + document.documentElement.clientHeight
+			};
+
+		if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+			targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+			targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+			targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+			// Если элемент полностью видно, то запускаем следующий код
+			count1.start();
+			count2.start();
+			count3.start();
+			count4.start();
+			count5.start();
+		};
+	};
+
+	// Запускаем функцию при прокрутке страницы
+	window.addEventListener('scroll', function() {
+		Visible (counterBlock);
+	});
+
+	// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+	Visible (counterBlock);
 
 };
 if (document.readyState !== 'loading') {
